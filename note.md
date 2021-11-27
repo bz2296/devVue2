6611d0f6b043532989dd186ecb8b456313983528
bz2296 <bz2296@163.com>
2021-11-27 19:48:57 +0800

    解决别名设置问题

---
+++ note.md
@@ -0,0 +1,19 @@
+日志：
+1.为了引入时忽略文件扩展名
+    新增依赖
+    eslint-import-resolver-webpack
+    来源： https://www.jianshu.com/p/fe96234d01cc
+    
+    结果： 问题已解决
+2.路径别名设置
+    修改文件
+        vue.config.js
+    来源
+        https://blog.csdn.net/MCQq123321/article/details/83348119
+    意外情况
+        上述来源中最后一步设置webpack配置是错的，导致本来正常的跳转失效，现通过来源2
+        设置回默认自动应用node_modules的webpack.config.js解决问题，但是并未使用具体路径
+        ，而是使用默认查找路径
+    来源2
+        https://blog.csdn.net/qq_37274323/article/details/100206842
+    
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
         <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <test></test>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { isIE } from '@tools';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@components/HelloWorld';
+import test from './Test';
 
 export default {
     name: 'Home',
     components: {
         HelloWorld,
+        test,
     },
     created() {
         console.log('asda', isIE);
